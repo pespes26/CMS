61492b1a01c934a46838f666753af8d3295b0b22
pespes26 <bar.pesso26@gmail.com>
2024-08-31 13:23:23 +0300

    Updated Migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)  # This generates a random secret key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:89562310@localhost:5433/coursesDB'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import routes, models
